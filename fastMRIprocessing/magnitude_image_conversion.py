1b2fe302685aedc2cc50183c51acec26ea35976f
trevor-chan <tjchan0@gmail.com>
2023-05-12 15:25:20 -0400

    large dataset conversion

--- fastMRIprocessing/magnitude_image_conversion.py
+++ fastMRIprocessing/magnitude_image_conversion.py
@@ -4,6 +4,7 @@ import os
 import glob
 from PIL import Image
 from multiprocessing import Pool
+import sigpy
 
 
 
@@ -12,8 +13,7 @@ from multiprocessing import Pool
 # sampling ratio is the density of kspace sampling along each dimension - (2,1) will correspond to 
 #      an image where the height of two rows equals the width of one column
 
-def process_h5_singlecoil(filepath,outshape=(256,256),sampling_ratio=(2,1),savedir='data_large'):
-    print(filepath)
+def process_h5_coilcombined_magnitude(filepath,outshape=(128,128),sampling_ratio=(2,1),savedir='data_128_coilcombined'):
     if savedir is None:
         savedir = os.path.split(filepath)[0]
     savename = os.path.splitext(os.path.split(filepath)[1])[0]
@@ -24,15 +24,14 @@ def process_h5_singlecoil(filepath,outshape=(256,256),sampling_ratio=(2,1),saved
         slices, coils, height, width = kspace.shape    #kspace opens as (z, coils, height, width)
         kspace_cropped = kspace[:,:,int((height/2)-(outshape[0]*sampling_ratio[0]/2)):int((height/2)+(outshape[0]*sampling_ratio[0]/2)):sampling_ratio[0], 
                                     int((width/2)-(outshape[1]*sampling_ratio[1]/2)):int((width/2)+(outshape[1]*sampling_ratio[1]/2)):sampling_ratio[1]].copy()
-    
+        
+        
         for zslice in range(kspace_cropped.shape[0]):
-            for coil in range(kspace_cropped.shape[1]):
-                array = np.stack((kspace_cropped[zslice,coil,:,:].real,
-                                  kspace_cropped[zslice,coil,:,:].imag), axis=-1)  #Saves in image channel format, (height, width, channel) to be permuted later
-                savepath = '{}/{}_{}_{}.npy'.format(savedir,savename,str(zslice).zfill(2),str(coil).zfill(2))
-                
-                with open(savepath, 'wb') as f:
-                    np.save(f, array)
+            
+            rss_array = np.sum(np.power(np.abs(sigpy.fft(kspace_cropped[zslice,:,:,:])),2),axis=0)
+            savepath = '{}/{}_{}.png'.format(savedir,savename,str(zslice).zfill(2))  
+            rss_array = ((rss_array * 6000000) * 255).clip(0, 255).astype(np.uint8)
+            Image.fromarray(rss_array, 'L').save(savepath)
             
             
 
@@ -41,7 +40,7 @@ def main():
     filelist = sorted(glob.glob(datasetpath))
 
     with Pool() as p:
-        p.map(process_h5_singlecoil, filelist)
-        
+        p.map(process_h5_coilcombined_magnitude, filelist)
+
 if __name__=="__main__":
     main()
